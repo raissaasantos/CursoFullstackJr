343756ddbf556dde8b47f8d8947b862ba95f8237
Raissa Assis Santos <raissasantos55@yahoo.com>
2024-04-25 19:46:08 -0300

    Update exercicio16.js

--- Modulo01/exercicio16.js
+++ Modulo01/exercicio16.js
@@ -1,3 +1,8 @@
+/*
+16. Escreva um algoritmo para imprimir os 50 primeiros número primos maior que 100.
+Obs.: Número primo é aquele divisível somente por 1 e ele mesmo.
+*/
+
 let contador = 0;
 let num = 2;
 
