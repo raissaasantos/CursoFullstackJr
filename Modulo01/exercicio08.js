950cef2d86a246b28d4317234c6795c6ea08777e
Raissa Assis Santos <raissasantos55@yahoo.com>
2024-04-25 19:39:34 -0300

    Update exercicio08.js

--- Modulo01/exercicio08.js
+++ Modulo01/exercicio08.js
@@ -1,3 +1,8 @@
+/*
+8. Escreva um algoritmo para ler 2 valores (considere que não serão lidos valores iguais)
+e escreve-los em ordem crescente.
+*/
+
 const prompt = require("prompt-sync")();
 
 let value1 = parseInt(prompt("Digite o primeiro valor: "));
