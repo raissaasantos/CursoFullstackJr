8b9a3eb49025cb1f30cffe533d8e0fbbc5639635
Raissa Assis Santos <raissasantos55@yahoo.com>
2024-04-25 19:38:27 -0300

    Update exercicio05.js

--- Modulo01/exercicio05.js
+++ Modulo01/exercicio05.js
@@ -1,3 +1,8 @@
+/*
+5. Acrescente ao exercício acima a mensagem ‘Você foi REPROVADO! Estude mais’ caso a
+média calculada seja menor que 6,0.
+*/
+
 const prompt = require("prompt-sync")();
 
 let nota1 = parseInt(prompt("Digite a primeira nota: "));
