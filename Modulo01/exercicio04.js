6e7b78d81e8fecedb4f004faae6754987844d1c0
Raissa Assis Santos <raissasantos55@yahoo.com>
2024-04-25 19:38:03 -0300

    Update exercicio04.js

--- Modulo01/exercicio04.js
+++ Modulo01/exercicio04.js
@@ -1,3 +1,10 @@
+/*
+4. Escreva um algoritmo para ler as notas das duas avaliações de um aluno no semestre,
+calcular e escrever a média semestral e a seguinte mensagem: ‘PARABÉNS! Você foi
+aprovado’, somente se o aluno foi aprovado (considere 6.0 a nota mínima para
+aprovação).
+*/
+
 const prompt = require("prompt-sync")();
 
 let nota1 = parseInt(prompt("Digite a primeira nota: "));
