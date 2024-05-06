a16bb2c93fcdb7bf00d1597979ee58071fd6291b
raissaasantos <raissasantos55@yahoo.com>
2024-05-06 14:11:05 -0300

    First exercise of List02

---
+++ modulo01/lista02/exercicio01.js
@@ -0,0 +1,18 @@
+function calculateLifeReduction(cigarettesPerDay, yearsSmoking) {
+    const minutesLostPerCigarette = 10;
+    const daysPerYear = 365;
+    const totalCigarettesSmoked = cigarettesPerDay * daysPerYear * yearsSmoking;
+    const totalMinutesLost = totalCigarettesSmoked * minutesLostPerCigarette;
+    const totalDaysLost = totalMinutesLost / (60 * 24); 
+
+    return totalDaysLost;
+}
+
+const prompt = require('prompt-sync')();
+
+const cigarettesPerDay = parseInt(prompt("Quantidade de cigarros fumados por dia: "));
+const yearsSmoking = parseInt(prompt("Há quantos está fumando: "));
+
+const daysLost = calculateLifeReduction(cigarettesPerDay, yearsSmoking);
+
+console.log("Você perdeu aproximadamente " + daysLost.toFixed(2) + " dias de vida fumando.");
