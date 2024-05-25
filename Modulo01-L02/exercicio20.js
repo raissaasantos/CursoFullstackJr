9d3ea0eded0bc761c1c364eb09e859b34ab686e9
raissaasantos <raissasantos55@yahoo.com>
2024-05-25 17:25:19 -0300

    20th exercise

--- Modulo01-L02/exercicio20.js
+++ Modulo01-L02/exercicio20.js
@@ -11,3 +11,32 @@ Salário líquido:
 (Dicas: desconto de 12%, salário líquido é a diferença entre salário bruto e a redução do
 INSS).
 */
+
+
+function calcularDescontoINSS(salarioBruto) {
+    return salarioBruto * 0.12; // Desconto de 12%
+}
+
+function calcularSalarioLiquido(salarioBruto, descontoINSS) {
+    return salarioBruto - descontoINSS;
+}
+
+
+const funcionarios = [
+    { matricula: 101, nome: "James", salarioBruto: 3000 },
+    { matricula: 102, nome: "Malu", salarioBruto: 4000 },
+  
+];
+
+
+funcionarios.forEach(funcionario => {
+    const descontoINSS = calcularDescontoINSS(funcionario.salarioBruto);
+    const salarioLiquido = calcularSalarioLiquido(funcionario.salarioBruto, descontoINSS);
+    
+    console.log("Matrícula: " + funcionario.matricula);
+    console.log("Nome: " + funcionario.nome);
+    console.log("Salário bruto: R$ " + funcionario.salarioBruto.toFixed(2));
+    console.log("Dedução INSS: R$ " + descontoINSS.toFixed(2));
+    console.log("Salário líquido: R$ " + salarioLiquido.toFixed(2));
+    console.log("--------------------------------------");
+});
